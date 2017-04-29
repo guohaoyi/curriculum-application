eab25ace56bf4f222a1f6d3d0927d5ff626c1120
Haoyi Guo <haoyi.guo@centre.edu>
2017-04-29 12:45:45 -0400

    Added GenerateGraph function, the graph will now update as the user selects different semesters

--- public/angular/controller/profile.js
+++ public/angular/controller/profile.js
@@ -11,7 +11,6 @@ app.controller('profileCtrl', function ($scope, $http, $location) {
     $http.get('/profile/userInfo').then(function (res) {
         $scope.courses = res.data[0];
         $scope.skills = res.data[1];
-	console.log($scope.skills);
 	var semesters = [];
 	for (var i = 0; i < res.data[0].length; i++) {
 	    var term = res.data[0][i].season.replace(/\s/g,'') + " " + res.data[0][i].year;
@@ -21,7 +20,6 @@ app.controller('profileCtrl', function ($scope, $http, $location) {
 	}
 	$scope.semesters = semesters;
 	$scope.currentcourses = res.data[0];
-	console.log($scope.currentcourses);
     });
 
     $http.get('/courseList').then(function (res) {
@@ -36,13 +34,16 @@ app.controller('profileCtrl', function ($scope, $http, $location) {
     {
 	var divs = $('.checks');
 	var courses = [];
+	var semesters = [];
 	
 	for (var i = 0; i < divs.length; i ++)
 	    {
+		
 		var divchildren = $(divs[i]).children();
 		if (divchildren[0].checked)
 		    {
-			console.log(divchildren[1]);
+			
+			semesters.push($(divchildren[1]).text());
 			for (var j = 0; j < $scope.courses.length; j++)
 			{
 			    
@@ -57,7 +58,7 @@ app.controller('profileCtrl', function ($scope, $http, $location) {
 	
 	$scope.currentcourses = [];
 	$scope.currentcourses = courses;
-	
+	generateGraph(semesters);
     }
     
     $scope.restore = function()
@@ -68,10 +69,109 @@ app.controller('profileCtrl', function ($scope, $http, $location) {
     function uncheckAll()
     {
 	var divs = $('.checks');
+	var semestesr = [];
 	for (var i = 0; i < divs.length; i++)
 	    {
 		var divchildren = $(divs[i]).children();
 		divchildren[0].checked = false;
+		semesters.push($(divchildren[1]).text());
 	    }
+	generateGraph(semesters);
+	
     }
+
+
+    function generateGraph(semesters)
+    {
+	function getAllSkills()
+	{
+	    return $.ajax({
+		url: '/skillList',
+		type: 'GET',
+		async: false,
+		success: function(result){
+		    skills = result;
+		}
+	    });
+	}
+	function getCourseSkills()
+	{
+	    return $.ajax({
+		url: '/skillInfo',
+		type: 'GET',
+		async: false,
+		success: function(result){
+		    course_skills = result;
+		}
+	    });
+	}
+
+	var skills = null;
+	var course_skills = null;
+	getAllSkills()
+	getCourseSkills();
+	var aggregate = [];
+
+	for(var i=0;i<skills.length;i++)
+	{
+	    aggregate[skills[i].skill]=0;
+	}
+	
+
+
+	var learned_skills = [];
+
+	for(var i=0;i<semesters.length;i++)
+	{
+	    var temp = [];
+	    learned_skills.push(temp);
+	    for(var j=0;j<course_skills.length;j++)
+	    {
+		if(semesters[i]==course_skills[j].season.replace(/\s/g,'') + " " + course_skills[j].year)
+		{
+		    var containsSkill = learned_skills[i].filter(function(course) { return course.axis == course_skills[j].skill; })
+		    if(containsSkill.length>0)
+		    {
+			containsSkill[0].value = course_skills[j].score + containsSkill[0].value;
+			aggregate[course_skills[j].skill] +=course_skills[j].score;
+		    }
+		    else
+		    {
+			var json = JSON.parse('{"axis":"' + course_skills[j].skill + '","value":' + course_skills[j].score + '}');
+			learned_skills[i].push(json);
+			aggregate[course_skills[j].skill] +=course_skills[j].score;
+		    }
+		}
+	    }
+	}
+	//adds 0's?
+	for(var i=0;i<skills.length;i++)
+	{
+	    for(var j=0;j<semesters.length;j++)
+	    {
+		var containsSkill = learned_skills[j].filter(function(course) { return course.axis == skills[i].skill; })
+		if(containsSkill.length==0)
+		{
+		    var json = JSON.parse('{"axis":"' + skills[i].skill + '","value":0}');
+                    learned_skills[j].push(json);
+		}
+	    }
+	}
+
+	var total =[];
+	for(var i=0;i<skills.length;i++)
+	{
+	    var skill  = skills[i].skill;
+	    var val = aggregate[skill];
+
+	    var obj = {skill:skill,value:val};
+	    total.push(obj);
+	}
+
+	RadarScript(["total"],[total],skills);
+    }
+
+
+
+
 });
