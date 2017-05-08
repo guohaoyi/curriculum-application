27d800a03a249080ca04eaed2b19254a8c57f8e6
Haoyi Guo <haoyi.guo@centre.edu>
2017-05-07 23:37:50 -0400

    Edited E2E test file, added unit test file

--- tests/e2e/e2e.tests.js
+++ tests/e2e/e2e.tests.js
@@ -1,11 +1,16 @@
 describe('Curriculum App E2E Tests:', function() {
-    describe('Insertion description here', function() {
-	it('Should be able to login', function() {
-	    browser.get('http://localhost:3333/login');
-	    element(by.css('button#myButton')).click();
-	    element(by.binding('error')).getText().then(function(errorText) {
-		expect(errorText).toBe('There is a duplicate or an empty skill. Please adjust before submitting');
-	    });
+    describe('Home Page', function() {
+	it('Should let me go to the login page', function() {
+	    browser.get('http://localhost:3333');
+	    browser.ignoreSynchronization = true;
+	    element(by.css('a.btn.btn-default')).click();
+	    expect(browser.getCurrentUrl()).toEqual('http://localhost:3333/login');
+	});
+	it('Should be able to login with the test account', function() {
+	    element(by.name('email')).sendKeys('test@test.test');
+	    element(by.css('input[type=password]')).sendKeys('test');
+	    element(by.css('button[type=submit]')).click();
+	    expect(browser.getCurrentUrl()).toEqual('http://localhost:3333/login');
 	});
     });
 });
